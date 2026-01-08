b09eba170cf3dfa70d97aec7edaadd68b4b6505a
abram axel booth <boothaa@gmail.com>
2026-01-08 10:29:50 -0500

    wip

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-import mock
+from unittest import mock
 import pytest
 
 from elasticsearch6_dsl import connections
--- tests/settings.py
+++ tests/settings.py
@@ -9,7 +9,6 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE_CLASSES = []
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
-TIME_ZONE = "UTC"
 ELASTICSEARCH_DSL = {
     "default": {"hosts": os.environ.get("ELASTICSEARCH_HOST", "localhost:9201")}
 }
